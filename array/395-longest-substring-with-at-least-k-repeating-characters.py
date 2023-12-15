a3dca29392c891ae22d0070378a168e1a8dd4b24
Cathleen Corr <c.corr.dev@gmail.com>
2023-12-15 08:35:54 -0600

    strategizing

--- array/395-longest-substring-with-at-least-k-repeating-characters.py
+++ array/395-longest-substring-with-at-least-k-repeating-characters.py
@@ -46,6 +46,43 @@ well, if I could sort by values in ascending order, then I would always know, by
 the problem here is that it looks like in python, there's no dict structure that maintains sorted order by value
 coming back to this after some time away... if ANY character in the window has a count below k, then the window is invalid.
 coming back again... okay. i don't think it's actually brute force because the max size of the hash table would be 26 since english has 26 characters
+thinking some more:
+okay, the way I have it right now would kind of be heading in the direction of looking for the shortest, but i'm looking for the longest. so, essentially, to keep the window invariant... when should i advance left?
+well, I should advance left while it is NOT valid, meaning that some counts are less than k
+but the problem here, I think, is that from the start, it does not meet the invariant. so left will always be growing.
+so actually i guess the condition to advance left should be if previously it was valid, but the characte
+r we added at the right made it invalid
+
+coming back:
+let's say your window is "aabb" and k is 2
+how do you know whether to continue growing the window right - could be like "aabbc" or "aabbcc"
+well I guess you could keep growing, and only move left if you encounter something like "aabbcaaabb"
+so then you could move left at the point where you have a portion of your window that is longer than the previous longest portion. but how would you track this?
+well when longest = 0 and window = "aabb"
+then you set longest = 4 because everything from the start to the end index has a count of 2
+but when you encounter "aabbc" you don't update the window because c only has count 1
+and only once you encounter the full thing "aabbcaaabb", would you say longest = 5 and move left to one ahead of c, so you have "aaabb"
+again - how are you trakcing this?
+well, in a hash table, you could track the count of each character. so we would know each has a count of 2 except for c which has a count of 1, with the window "aabbcaaabb". but how would we know which are adjacent? knowing only the count, the string could be like "aabbaaabbc", for all we know
+what if I also maintainned some type of list data structure, and i only add characters to the list once the number of adjacent ones >= k?
+so for window "aabbcaaabb" i could have list ["aabb", "aaabb"]. and actuallly what the list might look like is just a list of indexes, for the sake of storage, like (i, j). and so i guess i could update longest when i have an i, j
+that is longer than a previous one
+but how to remove from the list?
+hm, this is getting more complex than I like. How can I simplify the problem?
+what if instead of doing the above approach, i check for 1 adjacent character that is >= k? then 2 adjacent characters that are >= k? all the way to 26.
+so that might look like:
+for 1:
+track the count in a hashtable
+"aaa"
+if count of hashtable == 1 and all are >= k, update longest
+move left when count of hash table would exceed 1
+for 2:
+"aabbc"
+move left when count in hash table > 2
+otherwise if each count >= k update longest
+and so on...
+so what this does is simplify the problem statement from
+"find the longest substring with at least k repeating characters" to "find the longest substring consisting of exactly 1 unique character(s) with at least k repeating characters"..."find the longest substring consisting of exactly 2 unique character(s) with at least k repeating characters"... and so on...
 
 test:
 I think I walked thru enough of a test above
