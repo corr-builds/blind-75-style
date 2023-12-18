b368d5a9c6f17e4ab4c463c29b9ff388c53052af
Cathleen Corr <c.corr.dev@gmail.com>
2023-12-18 08:46:46 -0600

    PASSED

---
+++ array/438-find-all-anagrams-in-a-string-started-12-18-2023.py
@@ -0,0 +1,61 @@
+"""
+clarify:
+so an anagram is a rearangement of the letters
+so one way to do this is generate all the anagrams of p, then search for each of them in s
+there's got to be a better way to do this
+well, suppose we use sliding window
+I suppose we could maintain some sort of list of the letters in p, or maybe a map of the letters in p to their count
+then when we could also have a map that represents the count of characters in our window
+when we expand the window, we could add a character to the window hashmap.
+- if that character that was added to the window hashmap does not exist in p's hash table, then window is invalid, and we should shrink until it's valid
+- the window is also invalid if the character count in the actual window exceeds the count for that character in p's hash table
+
+input:
+s - str
+p - str
+
+output:
+all start indices of p's anagrams in s
+can be in any order
+
+edge case:
+s == p
+
+strategy:
+the strategy was pretty much outlined above
+assignment for after: what is the big O of checking whether a key exists in a hash table? (dict)
+
+test:
+s = "aab"
+p = "ab"
+p_dict = {a:1, b:1}
+window = {a:1}
+window = {a:2}
+it's invalid so
+window = {a:1}
+window = {a:1, b:1}
+window == p_dict, so
+add to our result list
+result = [1] <- start index
+
+"""
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        result = []
+        left = 0
+        goal = defaultdict(int)
+        for c in p:
+            goal[c] += 1
+        window = defaultdict(int)
+        for right, c in enumerate(s):
+            window[c] += 1
+            while c in window and not c in goal or goal[c] < window[c]:
+                window[s[left]] -= 1
+                if window[s[left]] == 0:
+                    del window[s[left]]
+                left += 1
+            if goal==window:
+                result.append(left)
+        return result
+        
