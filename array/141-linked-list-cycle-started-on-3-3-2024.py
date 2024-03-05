cf162eced28188fb5532d58f00b66b4c25b8794b
Cathleen Corr <c.corr.dev@gmail.com>
2024-03-05 02:19:38 -0600

    another option - hashing the node

--- array/141-linked-list-cycle-started-on-3-3-2024.py
+++ array/141-linked-list-cycle-started-on-3-3-2024.py
@@ -1,19 +1,10 @@
-Beats 23, 81
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return False
-        fast = head.next
-        while head and fast and fast.next:
-            if head == fast:
+        d = {}
+        while head:
+            if head in d:
                 return True
+            else:
+                d[head] = 0
             head = head.next
-            fast = fast.next.next
-        return False 
+        return False
