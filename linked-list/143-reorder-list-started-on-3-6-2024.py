2e2a6b1a30b9f84905054728cfb21379d62831fb
Cathleen Corr <c.corr.dev@gmail.com>
2024-03-07 21:51:00 -0600

    passing

--- linked-list/143-reorder-list-started-on-3-6-2024.py
+++ linked-list/143-reorder-list-started-on-3-6-2024.py
@@ -63,35 +63,35 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
-            return None
-        # put nodes in stack
-        stack = []
-        left = head
-        while left:
-            stack.append(left)
-            left = left.next
+        # find the midpoint
+        slow, fast = head, head.next
+        # odd - s f x, x s x (f is at null) <- so slow.next will be start of second half
+        # even - s f x x, x s x f <- so slow.next will be start of second half
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        """while second.next:
-            first.next, first = second, first.next
-            second.next, second = first, second.next
-        """
+        # reverse second half of list
+        secondhead = slow.next # create head for second list
+        slow.next = None # make tail of left list none
+        prev = None
+        cur = secondhead
+        while cur:
+            orig_cur_next = cur.next # temp variable
+            cur.next = prev # reverse link
+            prev = cur
+            cur = orig_cur_next
 
+        # interlace
+        secondcur = prev
+        firstcur = head
+        while secondcur: # while secondcur works because second list will be same length or shorter, and we want to stop when one of them is null
+            tmpfirst, tmpsecond = firstcur.next, secondcur.next # store copies of the current next links on either list (L1 and Ln-1, initially)
+            firstcur.next = secondcur # make item from first point to item from second
+            secondcur.next = tmpfirst # make item now pointed to from second list point to next item from first list (which we preserve using a temp variable)
+            firstcur, secondcur = tmpfirst, tmpsecond # advance to next items in list
+        # we can kind of think of the above logic as working on two "links" per iteration. link a to b, link b to c. repeat
 
-        # re-arrange pointers in spiral fashion
-        left = head
-        right = stack.pop()
-        print(stack)
-        while left != right:
-            left.next = right
-            print("connect " + str(left) + " to " + str(right))
-            left = left.next
-            print("connect " + str(right) + " to " + str(left))
-            right.next = left
-
-            right = stack.pop()
-
-        left.next = None # important step - when they meet, connect the middle of the spiral to null since it's the new end
         return head
 
         
